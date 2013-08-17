a4e9dcfe77c9bc6dd57b8e8b0ec68b8feb37a2f5
Leask Huang <i@leaskh.com>
2013-08-17 15:22:34 +0800

    adding imagealphamask function

--- libimage.php
+++ libimage.php
@@ -284,4 +284,39 @@ class libImage {
         return false;
     }
 
+
+    public function imagealphamask(&$picture, $mask) {
+        // Get sizes and set up new picture
+        $xSize = imagesx($picture);
+        $ySize = imagesy($picture);
+        $newPicture = imagecreatetruecolor($xSize, $ySize);
+        imagesavealpha($newPicture, true);
+        imagefill($newPicture, 0, 0, imagecolorallocatealpha($newPicture, 0, 0, 0, 127));
+
+        // Resize mask if necessary
+        if ($xSize !== imagesx($mask) || $ySize !== imagesy($mask)) {
+            $tempPic = imagecreatetruecolor($xSize, $ySize);
+            imagecopyresampled($tempPic, $mask, 0, 0, 0, 0, $xSize, $ySize, imagesx($mask), imagesy($mask));
+            imagedestroy($mask);
+            $mask = $tempPic;
+        }
+
+        // Perform pixel-based alpha map application
+        for ($x = 0; $x < $xSize; $x++) {
+            for ($y = 0; $y < $ySize; $y++) {
+                $alpha = imagecolorsforindex($mask, imagecolorat($mask, $x, $y));
+                $color = imagecolorsforindex($picture, imagecolorat($picture, $x, $y));
+                $alpha = 127 - floor((127-$color['alpha']) * ($alpha['red'] / 255));
+                if (127 === $alpha) {
+                    continue;
+                }
+                imagesetpixel($newPicture, $x, $y, imagecolorallocatealpha($newPicture, $color['red'], $color['green'], $color['blue'], $alpha));
+            }
+        }
+
+        // Copy back to original picture
+        imagedestroy($picture);
+        $picture = $newPicture;
+    }
+
 }
